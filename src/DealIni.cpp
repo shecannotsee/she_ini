f08d5054db401c1fa284235d7278d6bbaf6fa6c7
shecannotsee <shecannotsee@163.com>
2023-01-07 01:35:24 +0800

    feat:添加处理ini的类架构

--- src/DealIni.cpp
+++ src/DealIni.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "DealIni.h"
+
+sheIni::DealIni::DealIni()
+    : result_(INI_line_state::defaultValue),
+      section_(std::string()),
+      key_(std::string()),
+      value_(std::string()),
+      type_(sheIni::INI_value_type::defaultValue){
+};
+
+sheIni::FSM_state sheIni::DealIni::interface(char ch) {
+  if ( ch==(int)INI_char_state::notes_numberSign ) {
+
+  } else if ( ch == (int)INI_char_state::section_start ) {
+
+  } else if ( ch == (int))
+}
+
--- src/DealIni.h
+++ src/DealIni.h
@@ -1,23 +1,52 @@
 //
 // Created by  shecannotsee on 2023/1/7.
 //
-// 该函数是处理ini具体字符的类
+// 该类是处理ini具体字符的类
+// 该类的一切行为和成员变量都和ini文件的属性有关
 //
 
 #ifndef SHEINI_DEALINI_H
 #define SHEINI_DEALINI_H
 
+#include <string>
+#include <tuple>
+#include <FSMState.h>
 #include <IniState.h>
 
+namespace sheIni {
 class DealIni {
-  static void singleNote();
-  static void multipleSection();
-  static void multipleType();
-  static void singleEqual();
-  static void singleLineEnd();
-  static void singleLinuxLineBreak();
-  static void multipleWindowsLineBreak();
-};
+ public:
+  DealIni();
+  ~DealIni() = default;
+  // Disable copy
+  DealIni(const DealIni&) = delete;
+  DealIni& operator=(const DealIni&) = delete;
+  // Disable move
+  DealIni(DealIni&&) = delete;
+  DealIni& operator=(DealIni&&) = delete;
+
+ private:
+  INI_line_state          result_;    // 主要确定返回的内容
+  std::string             section_;   // ini:section
+  std::string             key_;       // ini:key
+  std::string             value_;     // ini:value
+  sheIni::INI_value_type  type_;      // ini extend:value type
+
+ public:
+  FSM_state interface(char);
+  FSM_state singleNote();
+  FSM_state multipleSection();
+  FSM_state multipleType();
+  FSM_state singleEqual();
+  FSM_state singleLineEnd();
+  FSM_state singleLinuxLineBreak();
+  FSM_state multipleWindowsLineBreak();
+
+ public:
+  std::tuple<INI_line_state,INI_value_type,std::string,std::string> get();
+
+};// class DealIni
+};// namespace sheIni
 
 
 #endif //SHEINI_DEALINI_H
