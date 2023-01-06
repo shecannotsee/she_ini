88e062c36f33c5d7ff99f5df1405d8a5560be89e
shecannotsee <shecannotsee@163.com>
2023-01-07 00:43:50 +0800

    test:添加测试代码

--- test/method_3_FSM_test.h
+++ test/method_3_FSM_test.h
@@ -5,10 +5,28 @@
 #ifndef SHEINI_METHOD_3_FSM_TEST_H
 #define SHEINI_METHOD_3_FSM_TEST_H
 
+#include <fstream>
+#include <iostream>
+#include <FSM.h>
+
 namespace method_3_FSM_test {
 
 void main () {
+  std::ifstream ini_file("../IniTestFile.ini");
+  if (!ini_file.is_open()) {
+    std::cout << "Failed to open INI file" << std::endl;
+    return;
+  }
 
+  sheIni::FSM tool;
+
+  for (char ch;!ini_file.eof();) {
+    if (tool.isEnd(ch)== true) {
+      // 一个阶段处理完毕，可以展示数据了
+    } else {
+      // 还没到处理边界
+    };
+  };// for
 
 
 };
