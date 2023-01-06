6bf479a1dcc74b4b8af3552223338ed3800042de
shecannotsee <shecannotsee@163.com>
2023-01-07 00:14:27 +0800

    feat:添加FSM的内部状态

---
+++ src/FSMState.h
@@ -0,0 +1,18 @@
+//
+// Created by shecannotsee on 2022/12/20.
+//
+
+#ifndef SHEINI_SRC_STATE_H_
+#define SHEINI_SRC_STATE_H_
+
+namespace sheIni {
+
+// 用该状态来对应fsm里如何运行
+enum class FSM_state : unsigned int {
+  Sizzle = 0,
+  Stop
+};
+
+};// namespace sheIni
+
+#endif //SHEINI_SRC_STATE_H_
