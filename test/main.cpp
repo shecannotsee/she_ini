63b062877cd2685b9b5113711512fb75e7bb5861
shecannotsee <shecannotsee@163.com>
2023-01-07 00:14:00 +0800

    test:添加测试代码

--- test/main.cpp
+++ test/main.cpp
@@ -6,11 +6,13 @@
 
 #include "method_1.h"
 #include "method_2.h"
+#include "method_3_FSM_test.h"
 
 int main() {
   std::cout<<"sheIni test start.\n";
   //method_1::main();
   method_2::main();
+  method_3_FSM_test::main();
   std::cout<<"sheIni test end.\n";
   return 0;
 };
---
+++ test/method_3_FSM_test.h
@@ -0,0 +1,18 @@
+//
+// Created by  shecannotsee on 2023/1/7.
+//
+
+#ifndef SHEINI_METHOD_3_FSM_TEST_H
+#define SHEINI_METHOD_3_FSM_TEST_H
+
+namespace method_3_FSM_test {
+
+void main () {
+
+
+
+};
+
+};// namespace method_3_FSM_test
+
+#endif //SHEINI_METHOD_3_FSM_TEST_H
