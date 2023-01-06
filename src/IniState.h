d735114f8bec03ea220a96f15cadcb546108859c
shecannotsee <shecannotsee@163.com>
2023-01-07 01:36:47 +0800

    feat:修改ini的状态以及修改各种枚举类的名字,令其更加贴合其含义

--- src/IniState.h
+++ src/IniState.h
@@ -8,14 +8,16 @@
 namespace sheIni {
 
 // 核心状态中type里的分类
-enum class type : unsigned int {
-  filePath = 0,   // file path
-  FPN,            // float,Floating point number
-  integer         // int or more bits int
+enum class INI_value_type : unsigned int {
+  defaultValue = 0 ,
+  filePath,           // file path
+  FPN,                // float,Floating point number
+  integer,            // int or more bits int
+  error
 };
 
-// 核心状态
-enum class INI_state : unsigned int {
+// 核心字符状态
+enum class INI_char_state : unsigned int {
   notes_numberSign = 0,           // '#',It means to use # to express comments
   section_start = 91,             // '['
   section_end = 93,               // ']'
@@ -25,9 +27,18 @@ enum class INI_state : unsigned int {
   lineEnd = 59,                   // ';'
   linuxLineBreak = 10,            // '\n'
   WindowsLineBreak_first = 13,    // '\r'
-  WindowsLineBreak_second         // '\n'
+//WindowsLineBreak_second,/*=10*/ // '\n'
+  other
+};// INI_char_state
 
-};// INI_state
+// INI每一行的状态
+enum class INI_line_state : unsigned int {
+  defaultValue = 0,
+  section,
+  noTypeKV,
+  TypeKV,
+  error
+};// INI_line_state
 
 };// namespace sheIni
 
