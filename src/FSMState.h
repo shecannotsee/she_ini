934fc94f905d80d701f7936e86dd520c4afba373
shecannotsee <shecannotsee@163.com>
2023-01-07 13:20:52 +0800

    feat:添加一些未来可扩展的状态

--- src/FSMState.h
+++ src/FSMState.h
@@ -14,6 +14,13 @@ enum class FSM_state : unsigned int {
   ERROR
 };
 
+// TODO:需要根据世纪业务对该类型进行更细致的处理,
+//      该状态表示FSM的输入,也许是一个字符,一个类或者其他的东西,
+//      或许要对
+enum class FSM_input_type : unsigned char {
+  singleChar = 1
+};
+
 };// namespace sheIni
 
 #endif //SHEINI_SRC_STATE_H_
