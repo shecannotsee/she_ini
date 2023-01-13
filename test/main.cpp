c002e2572e7c5cb6debf53209b52545ca0fbcd6f
shecannotsee <shecannotsee@163.com>
2023-01-13 13:49:18 +0800

    test:添加新增测试的使用用例

--- test/main.cpp
+++ test/main.cpp
@@ -7,12 +7,14 @@
 #include "method_1.h"
 #include "method_2.h"
 #include "method_3_FSM_test.h"
+#include "method_4_for_every_char.h"
 
 int main() {
   std::cout<<"sheIni test start.\n";
 //  method_1::main();
 //  method_2::main();
   method_3_FSM_test::main();
+//  method_4_for_every_char::main();
   std::cout<<"sheIni test end.\n";
   return 0;
 };
