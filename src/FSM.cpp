d1c06b0bd4883d87f142221245d5db1dbf34e53c
shecannotsee <shecannotsee@163.com>
2023-01-13 13:51:01 +0800

    fix:修改了状态机返回状态有限的情况,现在能够获取状态机的所有状态了

--- src/FSM.cpp
+++ src/FSM.cpp
@@ -15,14 +15,8 @@ void sheIni::FSM::add(char ch) {
   state_ = dealIni_->interface(ch);
 };
 
-bool sheIni::FSM::isEnd(char ch) {
-  if (state_==FSM_state::Sizzle) {
-    return false;
-  } else if (state_==FSM_state::Stop) {
-    return true;
-  } else if (state_==FSM_state::ERROR) {
-    return true;
-  }
+sheIni::FSM_state sheIni::FSM::getState() {
+  return this->state_;
 }
 
 std::tuple<sheIni::INI_line_state, sheIni::INI_value_type, std::string, std::string> sheIni::FSM::get() {
