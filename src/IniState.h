3efbbd2e4969e966369cf54f53143e03d317e598
shecannotsee <shecannotsee@163.com>
2023-01-13 13:51:42 +0800

    feat:添加了一组状态,现在可以更加清晰的划分ini在解析过程中的各种状态了

--- src/IniState.h
+++ src/IniState.h
@@ -7,7 +7,30 @@
 
 namespace sheIni {
 
-// 核心状态中type里的分类
+// INI每一行的类型,是注释
+enum class INI_line_state : unsigned int {
+  error = 0,          // 错误,一般在解析失败的时候会用到
+  note,               // 该行是注释
+  section,            // 该行是section
+  noTypeValue,        // 该行是一个没有类型的k-v
+  TypeValue,          // 该行是一个有类型的k-v
+  defaultValue        // 默认初始化用值,也许该行什么也没有
+};// INI_line_state
+
+// 在读取过程中的存储指针指向
+enum class INI_reading_pointer : unsigned int {
+  error = 0,
+  note,           // 注释,一般在测试时会用到,实际不会去做注释的解析操作
+  section,        // section
+  type,           // k-v的type
+  key,            // key
+  value,          // value
+  maybe_end,      // 可能是结束符,主要针对windows上的/r/n这种换行模式
+  end,            // 结束符
+  defaultValue    // 默认初始化用值
+};
+
+// 在判断出该行是kv并且带有类型时,具体类型(也与ascii码相对应)的枚举
 enum class INI_value_type : unsigned int {
   error = 0 ,
   defaultValue,        // 默认初始化用值,
@@ -16,7 +39,7 @@ enum class INI_value_type : unsigned int {
   integer = 105        // 'i',int or more bits int
 };
 
-// 核心字符状态
+// 核心字符状态,将字符与对应ascii字符码对应
 enum class INI_char_state : unsigned int {
   notes_numberSign = 0,           // '#',It means to use # to express comments
   section_start = 91,             // '['
@@ -32,18 +55,6 @@ enum class INI_char_state : unsigned int {
   other
 };// INI_char_state
 
-// INI每一行的状态
-enum class INI_line_state : unsigned int {
-  error = 0,          // 错误,一般在解析失败的时候会用到
-  note,               // 注释
-  section,            // section值
-  key,                // key(std::string)
-  noTypeValue,        // 无类型的value(std::string)
-  TypeValue,          // 有类型的value
-  mayBeEnd,           // 对于'\r'的处理
-  end,                // 结束符号
-  defaultValue        // 默认初始化用值
-};// INI_line_state
 
 };// namespace sheIni
 
