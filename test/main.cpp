ca2812e5ef79683816dcdc1bd757ecb16db26fb6
shecannotsee <shecannotsee@163.com>
2023-01-21 19:18:32 +0800

    test:添加测试代码

--- test/main.cpp
+++ test/main.cpp
@@ -9,6 +9,7 @@
 #include "method_3_FSM_test.h"
 #include "method_4_for_every_char.h"
 #include "method_5_magic_enum_test.h"
+#include "method_6_IniStructure_test.h"
 
 int main() {
   std::cout<<"sheIni test start.\n";
@@ -17,6 +18,7 @@ int main() {
   method_3_FSM_test::main();
 //  method_4_for_every_char::main();
 //  method_5_magic_enum_test::main();
+//  method_6_IniStructure::main();
   std::cout<<"sheIni test end.\n";
   return 0;
 };
--- test/method_6_IniStructure_test.h
+++ test/method_6_IniStructure_test.h
@@ -5,9 +5,14 @@
 #ifndef SHEINI_METHOD_6_INISTRUCTURE_TEST_H
 #define SHEINI_METHOD_6_INISTRUCTURE_TEST_H
 
+#include <IniStructure.h>
+
 namespace method_6_IniStructure {
 
 void main() {
+  sheIni::IniStructure IniTestFile_ini;
+  IniTestFile_ini.readFromFile("../IniTestFile.ini");
+  auto key1 = IniTestFile_ini.section("section1").getKey("key1");
 
 };
 
