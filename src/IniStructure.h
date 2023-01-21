c5ee0dfd745c9ed69aa2a1826d172963e3541094
shecannotsee <shecannotsee@163.com>
2023-01-21 19:33:22 +0800

    feat:添加接口和数据结构

--- src/IniStructure.h
+++ src/IniStructure.h
@@ -4,17 +4,37 @@
 
 #ifndef SHEINI_INISTRUCTURE_H
 #define SHEINI_INISTRUCTURE_H
+
+#include <string>
+#include <unordered_map>
+#include <tuple>
+
 namespace sheIni {
 class IniStructure {
  public:
-  IniStructure() = default;
+  IniStructure();
   ~IniStructure() = default;
-  // copy
+  // Disable copy
   IniStructure(const IniStructure&) = delete;
   IniStructure& operator=(const IniStructure&) = delete;
-  // move
+  // Disable move
   IniStructure(IniStructure &&) = delete;
   IniStructure& operator=(IniStructure&&) = delete;
+
+ private:
+  using value    = std::tuple</*file*/std::string,/*d*/double,/*i*/int,/*common*/std::string>;
+  using map_kv   = std::unordered_map<std::string,value>;
+  using map_s_kv = std::unordered_map<std::string,map_kv>;
+  map_s_kv IniStructure_;
+  std::string section_;
+
+ public:
+  void readFromFile(const std::string& path);
+  IniStructure& section(const std::string& section);
+  std::string getKey(const std::string& key);
+
+
+
 
 };// class IniStructure
 };// namespace sheIni
