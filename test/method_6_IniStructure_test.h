99c9237fa412a8f395716d1ce447cf8a9b612e7b
shecannotsee <shecannotsee@163.com>
2023-01-22 13:09:26 +0800

    test:添加测试代码

--- test/method_6_IniStructure_test.h
+++ test/method_6_IniStructure_test.h
@@ -13,8 +13,15 @@ void main() {
   sheIni::IniStructure IniTestFile_ini;
   IniTestFile_ini.readFromFile("../IniTestFile.ini");
   try {
-    auto key1 = IniTestFile_ini.section("section1").getKey("key1");
-    std::cout << key1 << "\n";
+    std::string key1 = IniTestFile_ini.section("section1").getKey("key1");
+    std::cout << "[" <<key1 << "]\n";
+
+    std::string key2 = IniTestFile_ini.section("section2").getKey("key2");
+    std::cout << "[" <<key2 << "]\n";
+
+    std::string x = IniTestFile_ini.section("section3").getKey("key1");
+    std::cout << "[" <<x << "]\n";
+
   } catch (const std::runtime_error& e) {
     std::cout << "Caught a runtime_error exception: " << e.what() << '\n';
   }
