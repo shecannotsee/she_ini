b95b0410bd4f86148017210e967c0d17390f63bf
shecannotsee <shecannotsee@163.com>
2023-02-13 10:26:39 +0800

    feat:添加了完整的测试案例

---
+++ example/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+// You should use this head file.
+#include <IniStructure.h>
+
+int main() {
+  sheIni::IniStructure web_config;
+  // Load the file data to your memory object.
+  web_config.readFromFile("../config.ini");
+  // And use the results.
+  auto ip       = web_config.section("config-set").getKey("ip");
+  auto port     = web_config.section("config-set").getKey("port");
+  auto cfg_path = web_config.section("config-set").getKey("config_path");
+  auto log_path = web_config.section("config-set").getKey("log_path");
+
+  using ::std::cout;using ::std::endl;
+  // print
+  cout << "http://" << ip << ":" << port << endl;
+  cout << "Config peth :[" << cfg_path << "]\n";
+  cout << "log path :[" << log_path << "]\n";
+
+
+  return 0;
+};
