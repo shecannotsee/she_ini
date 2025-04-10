c4c615acf85211efe245830a0d3927cd1d111957
shecannotsee <shecannotsee@163.com>
2025-04-11 03:09:15 +0800

    [feat] 1.Update write

--- src/source_writer.cpp
+++ src/source_writer.cpp
@@ -1,5 +1,39 @@
-//
-// Created by shecannotsee on 25-4-10.
-//
-
 #include "source_writer.h"
+
+using namespace ini::detail;
+
+source_writer::source_writer(const std::string& file_path) noexcept {
+  file_.open(file_path, std::ios::binary | std::ios::out);
+  if (!file_.is_open()) {
+    const auto error = "Open file failed: " + file_path;
+    throw std::runtime_error(error);
+  }
+}
+
+source_writer::~source_writer() {
+  file_.close();
+}
+
+void source_writer::write(const char* data, const uint32_t size) noexcept {
+  if (file_ && data) {
+    file_.write(data, size);
+  }
+}
+
+void source_writer::write(const std::string& data) noexcept {
+  if (file_) {
+    file_.write(data.data(), data.size());
+  }
+}
+
+void source_writer::write(const std::vector<char>& data) noexcept {
+  if (file_) {
+    file_.write(data.data(), data.size());
+  }
+}
+
+void source_writer::fflush() noexcept {
+  if (file_) {
+    file_.flush();
+  }
+}
--- src/source_writer.h
+++ src/source_writer.h
@@ -1,9 +1,27 @@
 #ifndef SHE_INI_SOURCE_WRITER_H
 #define SHE_INI_SOURCE_WRITER_H
 
+#include <fstream>
+#include <vector>
+
 namespace ini::detail {
 
-class source_writer {};
+class source_writer {
+  std::ofstream file_;
+
+ public:
+  explicit source_writer(const std::string& file_path) noexcept;
+
+  ~source_writer();
+
+  void write(const char* data, uint32_t size) noexcept;
+
+  void write(const std::string& data) noexcept;
+
+  void write(const std::vector<char>& data) noexcept;
+
+  void fflush() noexcept;
+};
 
 }  // namespace ini::detail
 
