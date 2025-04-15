9891e30e85e0e1e7afb1ce5c4ce6c9c228872ddd
shecannotsee <shecannotsee@163.com>
2025-04-15 16:17:54 +0800

    [test] 1.Add token filtering method test

--- test/unit/lexer/basic_functionality_test.cpp
+++ test/unit/lexer/basic_functionality_test.cpp
@@ -1,22 +1,12 @@
+#include <lexer.h>
 #include <she_test.h>
 
 TEST3(unit, lexer, basic_functionality_test) {
-  std::vector<std::string> line = {
-    {std::string("[admin]\n")},
-    {std::string("adress = aaaaaaaaaaaaaaaaaaaaaaaaaaaa\\\n         bbbbbbbbbbbb\n")},
-    {std::string("name = jac\\\n          k\n")},
-    {std::string("\n")},
-    {std::string("a\n")},
-    {std::string("des = asdsadasd\\asdasdasdas\\dasdasdsad")}
-  };
-  std::vector<std::string> line_without_line_break = {
-    {std::string("[admin]")},
-    {std::string("adress = aaaaaaaaaaaaaaaaaaaaaaaaaaaa\\\n         bbbbbbbbbbbb")},
-    {std::string("name = jac\\\n          k")},
-    {std::string("")},
-    {std::string("a")},
-    {std::string("des = asdsadasd\\asdasdasdas\\dasdasdsad")}
-  };
+  ini::detail::lexer test_lexer("../resource/escape.ini");
 
-
+  for (auto line = test_lexer.get_token(); !line.empty(); line = test_lexer.get_token()) {
+    for (const auto& c : line) {
+      std::cout <<"TEST:" <<c.value << std::endl;
+    }
+  }
 }
